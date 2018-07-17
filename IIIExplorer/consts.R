97143e218c10674a57fae9a703323fe34c98e139
Darren Maczka <dmaczka@vt.edu>
2018-07-16 21:54:48 -0400

    add consts.R

---
+++ IIIExplorer/consts.R
@@ -0,0 +1,18 @@
+grade_levels <- c("A", "A-",
+                  "B+", "B", "B-",
+                  "C+", "C", "C-",
+                  "D+", "D", "D-",
+                  "T",
+                  "F", "NR", "NG", "F *", "W", "WG", 
+                  "I", "P", "RP", "AUD", "X", "EQ", "S", "NS")
+
+# Limit to cases where a final grade is assigned
+valid_grades <- c("A", "A-", 
+                  "B+", "B", "B-", 
+                  "C+", "C", "C-", 
+                  "D+", "D", "D-", 
+                  "F", "F *",
+                  "NR", "NG",
+                  "W", "T")
+
+
